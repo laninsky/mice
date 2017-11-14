739a7b556b5199386c7a7886f17e1f8352cdd66a
Alana Alexander <laninsky@gmail.com>
2017-11-15 12:42:55 +1300

    Update postprocess.R

--- postprocess.R
+++ postprocess.R
@@ -1,10 +1,23 @@
-#Expecting the sample name as a prefix for the following files: two fasta files per sample (suffixed *.1.fa and *.2. fa), and a vcf file with the quality/phase of the various genotypes (suffixed *.phased_SNPs.vcf), and nothing else in the working directory
+#Expecting the following files (outputs from the previous step): 
+#combined_aligned.fasta - an aligned fasta file with the two mitogenomes titled *.1 and *.2.fa for each sample as well as the reference sample. Each sequence should only span one line
+#For each phased sample, a vcf file prefixed with sample name showing the quality/phase of the various genotypes (suffixed *.phased_SNPs.vcf)
 
 postprocess <- function(working_dir) {#1A
 #e.g. working_dir <- "/Users/alanaalexander/Dropbox/polg_mice"
 
-samplenames <- unique(gsub("\\..*","",list.files(working_dir)))
+samplenames <- unique(gsub(".phased_SNPs.vcf","",list.files(working_dir, pattern=".phased_SNPs.vcf")))
 
+input_fasta <- readLines(paste(working_dir,"/","combined_aligned.fasta",sep=""))
+nochars <- length(strsplit(input_fasta[2],"")[[1]])
+
+outputmat <- matrix(NA,nrow=(nochars+1),ncol=(length(seq(1,length(input_fasta),2))+length(samplenames)+1))
+outputmat[1,1:(length(seq(1,length(input_fasta),2)))] <- input_fasta[(seq(1,length(input_fasta),2))]
+  
+for (i in 1:(length(seq(1,length(input_fasta),2)))) {
+  outputmat[2:(dim(outputmat)[1]),(i+1)] <- strsplit(input_fasta[i*2],"")[[1]]
+}
+  
+  
 for (i in samplenames) {
   M1_name <- paste(i,".1.fa",sep="")
   M2_name <- paste(i,".2.fa",sep="")
