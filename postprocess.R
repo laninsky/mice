5a2ba33d0878c79ac520e3dd8977ebb7faa6cbca
Alana Alexander <laninsky@gmail.com>
2017-11-15 13:05:39 +1300

    Update postprocess.R

--- postprocess.R
+++ postprocess.R
@@ -5,17 +5,44 @@
 postprocess <- function(working_dir) {#1A
 #e.g. working_dir <- "/Users/alanaalexander/Dropbox/polg_mice"
 
+#Getting the sample names from the vcf files in the folder
 samplenames <- unique(gsub(".phased_SNPs.vcf","",list.files(working_dir, pattern=".phased_SNPs.vcf")))
 
+#Grabbing the aligned fasta files and figuring out the total number of nucleotides in the alignment
 input_fasta <- readLines(paste(working_dir,"/","combined_aligned.fasta",sep=""))
 nochars <- length(strsplit(input_fasta[2],"")[[1]])
 
+#creating an outputmatrix which will have a column for each sequence, the relative ref_pos, and whether the sites have been solidly phased per sample 
 outputmat <- matrix(NA,nrow=(nochars+1),ncol=(length(seq(1,length(input_fasta),2))+length(samplenames)+1))
 outputmat[1,1:(length(seq(1,length(input_fasta),2)))] <- input_fasta[(seq(1,length(input_fasta),2))]
-  
+
+#populating the rows with sequence from the fasta file  
 for (i in 1:(length(seq(1,length(input_fasta),2)))) {
   outputmat[2:(dim(outputmat)[1]),(i+1)] <- strsplit(input_fasta[i*2],"")[[1]]
 }
+
+#finding out what the reference column is (not present in the vcf sample names)
+ref_column <- which(!(gsub("\\..*","",gsub(">","",outputmat[1,]))[1:(length(seq(1,length(input_fasta),2)))] %in% samplenames))
+
+#creating a column giving the position relative to the reference sequence  
+outputmat[1,((length(seq(1,length(input_fasta),2)))+1)] <- "ref_pos"
+
+#populating this column - giving indel positions "compound" sites e.g. 484, 484_1, 484_2 so they are still relative to the reference  
+ref_site <- 1
+for (i in 2:(dim(outputmat)[1])) {
+  if(!(outputmat[i,ref_column]=="-")) {
+    outputmat[i,((length(seq(1,length(input_fasta),2)))+1)] <- ref_site
+    ref_site <- ref_site + 1
+    site_suffix <- 1
+  } else {
+    outputmat[i,((length(seq(1,length(input_fasta),2)))+1)] <- paste(ref_site,"_",site_suffix,sep="")
+    site_suffix <- site_suffix + 1
+  }
+}
+ 
+  
+  
+  
   
   
 for (i in samplenames) {
